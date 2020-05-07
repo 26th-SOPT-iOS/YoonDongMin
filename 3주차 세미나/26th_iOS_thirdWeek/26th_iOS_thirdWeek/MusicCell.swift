b40a65c341ab0d150e863e31e9228b3eb9f297d1
dongminyoon <ehdals8965@naver.com>
2020-05-07 23:22:45 +0900

    doc: 3주차 CollectionView 실습 업로드

---
+++ 3주차 세미나/26th_iOS_thirdWeek/26th_iOS_thirdWeek/MusicCell.swift
@@ -0,0 +1,23 @@
+//
+//  MusicCell.swift
+//  26th_iOS_thirdWeek
+//
+//  Created by 윤동민 on 2020/05/07.
+//  Copyright © 2020 윤동민. All rights reserved.
+//
+
+import UIKit
+
+class MusicCell: UICollectionViewCell {
+    static let identifier: String = "MusicCell"
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var singerLabel: UILabel!
+    
+    func set(_ musicInformation: Music) {
+        albumImageView.image = musicInformation.albumImg
+        titleLabel.text = musicInformation.musicTitle
+        singerLabel.text = musicInformation.singer
+    }
+}
